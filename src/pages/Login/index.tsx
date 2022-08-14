7177ed728928f759ee368d64176b565b853cb45b
Natallia Kostsina <natakostina123@gmail.com>
2022-08-14 13:03:49 +0200

    feat: add login form

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,11 @@
+import { Card } from 'antd';
 import React from 'react';
+import LoginForm from '../../components/LoginForm';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Card title="Login" style={{ width: 300 }}>
+      <LoginForm />
+    </Card>
   );
 }
